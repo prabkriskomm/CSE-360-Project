3eea91ff8ba3e121c4af3206ffd3c2a94c4ea74e
suhaas-pedapati <123433261+suhaas-pedapati@users.noreply.github.com>
2024-03-29 18:21:51 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+ @Override
+ public void start(Stage primaryStage) {
+     try {
+         StaffHomeTab staffHomeTab = new StaffHomeTab();
+         Scene scene = new Scene(staffHomeTab, 800, 600);
+         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+         primaryStage.setScene(scene);
+         primaryStage.setTitle("Mediate - Staff Home");
+         primaryStage.show();
+     } catch (Exception e) {
+         e.printStackTrace();
+     }
+ }
+
+ public static void main(String[] args) {
+     launch(args);
+ }
+}
