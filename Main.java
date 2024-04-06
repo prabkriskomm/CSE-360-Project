2fba6bbf8fd0600eade23c747ca6f6688fc38988
harsh-aggar <163202644+harsh-aggar@users.noreply.github.com>
2024-04-06 12:56:09 -0700

    Patient View - Patient Home Tab
    This is the patient home tab

--- Main.java
+++ Main.java
@@ -1,34 +1,60 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class Main extends Application {
- @Override
- public void start(Stage primaryStage) {
-     try {
-    	 
-    	 
-         //StaffHomeTab staffHomeTab = new StaffHomeTab();
-         //Scene scene = new Scene(staffHomeTab, 800, 600);
-         
-         PatientHomeTab patientHomeTab = new PatientHomeTab();
-         Scene scene = new Scene(patientHomeTab, 800, 600);
-         
-         
-         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-         primaryStage.setScene(scene);
-         primaryStage.setTitle("Mediate");
-         primaryStage.show();
-         
-     } catch (Exception e) {
-         e.printStackTrace();
-     }
- }
 
- public static void main(String[] args) {
-     launch(args);
- }
+	@Override
+	public void start(Stage primaryStage) {
+		// Root layout
+		BorderPane root = new BorderPane();
+
+		// MenuBar and Menus
+		MenuBar menuBar = new MenuBar();
+		Menu homeMenu = new Menu("Home");
+		Menu recordMenu = new Menu("Health Record");
+		Menu messageMenu = new Menu("Messages");
+		Menu visitMenu = new Menu("Visits");
+		menuBar.getMenus().addAll(homeMenu, recordMenu, messageMenu, visitMenu);
+
+		// HBox for aligning menu to the right
+		HBox menuBox = new HBox(menuBar);
+		menuBox.setAlignment(Pos.TOP_RIGHT); // Align to the right
+
+		// Welcome Message
+		Label welcomeLabel = new Label("Welcome, John!");
+		welcomeLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+
+		// Slogan
+		Label sloganLabel = new Label("MEDIATE\nEmpowering Health, One Click at a Time.");
+		sloganLabel.setStyle("-fx-font-size: 16px;");
+
+		// VBox for center alignment
+		VBox centerBox = new VBox(welcomeLabel, sloganLabel);
+		centerBox.setAlignment(Pos.CENTER);
+		centerBox.setSpacing(10);
+
+		// Set components to BorderPane
+		root.setTop(menuBar);
+		root.setCenter(centerBox);
+
+		// Scene and Stage setup
+		Scene scene = new Scene(root, 936, 530);
+		primaryStage.setTitle("MEDIATE - Patient Home");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
