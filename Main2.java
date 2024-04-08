6d0812440565fe6c9ea5ead64c3f4c3db6b41839
harsh-aggar <163202644+harsh-aggar@users.noreply.github.com>
2024-04-08 13:57:59 -0700

    Updated Patient Intake Form - Staff View
    Added a save info button which saves entered information as a .txt file

--- Main2.java
+++ Main2.java
@@ -9,8 +9,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main2 extends Application {
+
+    private TextField ageField, weightField, heightField, temperatureField, bpmField;
+    private TextArea healthConcernsArea;
+    private TableView<String> allergiesTable;
 
     @Override
     public void start(Stage primaryStage) {
@@ -37,13 +44,20 @@ public class Main2 extends Application {
         GridPane.setConstraints(vitalsTitle, 0, 0, 2, 1, HPos.CENTER, VPos.CENTER);
         vitalsGrid.getChildren().add(vitalsTitle);
 
+        ageField = new TextField();
+        weightField = new TextField();
+        heightField = new TextField();
+        temperatureField = new TextField();
+        bpmField = new TextField();
+
         String[] labels = {"Age", "Weight", "Height", "Temperature", "BPM"};
+        TextField[] fields = {ageField, weightField, heightField, temperatureField, bpmField};
         for (int i = 0; i < labels.length; i++) {
             Label label = new Label(labels[i]);
             label.setMinWidth(Region.USE_PREF_SIZE);
             label.setAlignment(Pos.CENTER_RIGHT);
 
-            TextField textField = new TextField();
+            TextField textField = fields[i];
             textField.setPrefWidth(200); // Set a preferred width for text fields
 
             GridPane.setConstraints(label, 0, i + 1);
@@ -59,7 +73,7 @@ public class Main2 extends Application {
         allergiesTitle.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
         allergiesTitle.setAlignment(Pos.CENTER);
         allergiesTitle.setMaxWidth(Double.MAX_VALUE);
-        TableView<String> allergiesTable = new TableView<>();
+        allergiesTable = new TableView<>();
         TableColumn<String, String> allergenColumn = new TableColumn<>("Allergen Name");
         TableColumn<String, String> dateColumn = new TableColumn<>("MM/YY");
         allergiesTable.getColumns().addAll(allergenColumn, dateColumn);
@@ -72,13 +86,21 @@ public class Main2 extends Application {
         healthConcernsTitle.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
         healthConcernsTitle.setAlignment(Pos.CENTER);
         healthConcernsTitle.setMaxWidth(Double.MAX_VALUE);
-        TextArea healthConcernsArea = new TextArea();
+        healthConcernsArea = new TextArea();
         healthConcernsBox.getChildren().addAll(healthConcernsTitle, healthConcernsArea);
 
         // Layout setup
         HBox contentBox = new HBox(20);
         contentBox.getChildren().addAll(vitalsGrid, allergiesBox, healthConcernsBox);
         contentBox.setAlignment(Pos.TOP_CENTER);
+
+        // Save button
+        Button saveButton = new Button("Save");
+        saveButton.setOnAction(event -> saveData());
+        VBox saveBox = new VBox(saveButton);
+        saveBox.setAlignment(Pos.BOTTOM_CENTER);
+        saveBox.setPadding(new Insets(15, 0, 15, 0));
+        contentBox.getChildren().add(saveBox);
 
         // Root layout
         BorderPane root = new BorderPane();
@@ -92,11 +114,41 @@ public class Main2 extends Application {
         primaryStage.show();
     }
 
+    private void saveData() {
+        try {
+            PrintWriter writer = new PrintWriter(new File("patient_data.txt"));
+
+            // Writing vitals to the file
+            writer.println("Vitals:");
+            writer.println("Age: " + ageField.getText());
+            writer.println("Weight: " + weightField.getText());
+            writer.println("Height: " + heightField.getText());
+            writer.println("Temperature: " + temperatureField.getText());
+            writer.println("BPM: " + bpmField.getText());
+
+            // Writing allergies to the file (assuming allergiesTable.getItems() returns the list of allergies)
+            writer.println("\nAllergies:");
+            for (String allergy : allergiesTable.getItems()) {
+                writer.println(allergy);
+            }
+
+            // Writing health concerns to the file
+            writer.println("\nHealth Concerns:");
+            writer.println(healthConcernsArea.getText());
+
+            writer.close();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Data saved successfully.");
+            alert.showAndWait();
+        } catch (FileNotFoundException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Could not save the data.");
+            alert.showAndWait();
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
 }
-
 
 
 
