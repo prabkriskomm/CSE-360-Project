6270c22b0e6d6d4a9013519cedb815edfcd633ad
movinya <93846023+movinya@users.noreply.github.com>
2024-04-08 22:58:26 -0700

    Update Patient.java set and organized all private variables

--- Patient.java
+++ Patient.java
@@ -4,9 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Patient {
-    private String patientId;
     private String patientName;
     private List<Visit> visits;
+    
+    //diagnosis
+    private String diagnosis;
+    private String courseOfAction;
+    private String prescription;
+    private String date;
+    
+    //intake
+    private int BP;
+    private int HR;
+    private int temp;
+    private double weight;
+    private double height;
+    private int age;
+    private String medicalHistory;
+
+    
+    //from Sign Up page 
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String insurance;
+    private String patientId;
+    private String firstName;
+    private String lastName;
+
 
     public Patient(String patientId, String patientName) {
         this.patientId = patientId;
