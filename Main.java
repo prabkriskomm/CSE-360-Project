6b557483e76fdae58a10b753242b4a95136400a2
movinya <93846023+movinya@users.noreply.github.com>
2024-04-10 11:19:38 -0700

    Update Main.java Main.java is not even used

--- Main.java
+++ Main.java
@@ -1,66 +1,66 @@
-//package application;
-////team 9 is amazing
-//import javafx.application.Application;
-//import javafx.stage.Stage;
-//import javafx.scene.Scene;
-//
-//public class Main extends Application {
-// @Override
-// public void start(Stage primaryStage) {
-//     try {
-//    	 
-////         StaffHomeTab staffHomeTab = new StaffHomeTab();
-////         Scene scene = new Scene(staffHomeTab, 800, 600);
-////         
-////         PatientHomeTab PatientHomeTab = new PatientHomeTab();
-////         Scene scene1 = new Scene(PatientHomeTab, 800, 600);
-//         
-//         scene1.gpackage application;
-//team 9 is amazing
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
+// //package application;
+// ////team 9 is amazing
+// //import javafx.application.Application;
+// //import javafx.stage.Stage;
+// //import javafx.scene.Scene;
+// //
+// //public class Main extends Application {
+// // @Override
+// // public void start(Stage primaryStage) {
+// //     try {
+// //    	 
+// ////         StaffHomeTab staffHomeTab = new StaffHomeTab();
+// ////         Scene scene = new Scene(staffHomeTab, 800, 600);
+// ////         
+// ////         PatientHomeTab PatientHomeTab = new PatientHomeTab();
+// ////         Scene scene1 = new Scene(PatientHomeTab, 800, 600);
+// //         
+// //         scene1.gpackage application;
+// //team 9 is amazing
+// import javafx.application.Application;
+// import javafx.stage.Stage;
+// import javafx.scene.Scene;
 
-public class Main extends Application {
- @Override
- public void start(Stage primaryStage) {
-     try {
+// public class Main extends Application {
+//  @Override
+//  public void start(Stage primaryStage) {
+//      try {
     	 
-         StaffHomeTab staffHomeTab = new StaffHomeTab();
-         Scene scene = new Scene(staffHomeTab, 800, 600);
+//          StaffHomeTab staffHomeTab = new StaffHomeTab();
+//          Scene scene = new Scene(staffHomeTab, 800, 600);
          
-         PatientHomeTab PatientHomeTab = new PatientHomeTab();
-         Scene scene1 = new Scene(PatientHomeTab, 800, 600);
+//          PatientHomeTab PatientHomeTab = new PatientHomeTab();
+//          Scene scene1 = new Scene(PatientHomeTab, 800, 600);
          
-         scene1.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-         primaryStage.setScene(scene1);
-         primaryStage.setTitle("Mediate - Staff Home");
-         primaryStage.show();
+//          scene1.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//          primaryStage.setScene(scene1);
+//          primaryStage.setTitle("Mediate - Staff Home");
+//          primaryStage.show();
          
          
-     } catch (Exception e) {
-         e.printStackTrace();
-     }
+//      } catch (Exception e) {
+//          e.printStackTrace();
+//      }
      
- }
+//  }
 
- public static void main(String[] args) {
-     launch(args);
- }
+//  public static void main(String[] args) {
+//      launch(args);
+//  }
  
-}etStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//         primaryStage.setScene(scene1);
-//         primaryStage.setTitle("Mediate - Staff Home");
-//         primaryStage.show();         
-//         
-//     } catch (Exception e) {
-//         e.printStackTrace();
-//     }
-//     
-// }
-//
-// public static void main(String[] args) {
-//     launch(args);
-// }
-// 
-//}
+// }etStylesheets().add(getClass().getResource("application.css").toExternalForm());
+// //         primaryStage.setScene(scene1);
+// //         primaryStage.setTitle("Mediate - Staff Home");
+// //         primaryStage.show();         
+// //         
+// //     } catch (Exception e) {
+// //         e.printStackTrace();
+// //     }
+// //     
+// // }
+// //
+// // public static void main(String[] args) {
+// //     launch(args);
+// // }
+// // 
+// //}
