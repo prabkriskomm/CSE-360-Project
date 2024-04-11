44b54d9f2523565313ccd45249910be91d605bc7
movinya <93846023+movinya@users.noreply.github.com>
2024-04-10 20:58:20 -0700

    Update Patient.java added file writing and reading for patient.txt

--- Patient.java
+++ Patient.java
@@ -1,30 +1,90 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Patient {
-    
+
     private List<Visit> visits;
-    
-    //intake
     private int age;
     private String medicalHistory;
-
-    //from Sign Up page 
     private String password;
     private String email;
     private String phoneNumber;
     private String insurance;
-    private String patientId;
     private String firstName;
     private String lastName;
     private String birthday;
+
+    // Constructor to initialize visits list
+    public Patient() {
+        this.visits = new ArrayList<>();
+    }
     
     public void addVisit(Visit visit) {
     	visits.add(visit);
     }
 
-    // Getter and Setter methods for visits
+    // Load patient data from patient.txt
+    public void loadData() {
+        try (BufferedReader br = new BufferedReader(new FileReader("patient.txt"))) {
+            //String header = br.readLine(); // Read and discard the header
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                this.firstName = values[0];
+                this.lastName = values[1];
+                this.birthday = values[2];
+                this.age = Integer.parseInt(values[3]);
+                this.medicalHistory = values[4];
+                this.password = values[5];
+                this.email = values[6];
+                this.phoneNumber = values[7];
+                this.insurance = values[8];
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Save patient data to patient.txt
+    public void saveData(){
+    	
+	    File file = new File("patient.txt");
+	    
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+    	    if (!file.exists()) {
+                file.createNewFile(); // Create a new file if it doesn't exist
+            }
+    	    
+            StringBuilder sb = new StringBuilder();
+            sb.append(this.firstName).append(",")
+              .append(this.lastName).append(",")
+              .append(this.birthday).append(",")
+              .append(this.age).append(",")
+              .append(this.medicalHistory).append(",")
+              .append(this.password).append(",")
+              .append(this.email).append(",")
+              .append(this.phoneNumber).append(",")
+              .append(this.insurance);
+            bw.write(sb.toString());
+            bw.newLine();
+            System.out.println(sb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Getter and Setter methods
     public List<Visit> getVisits() {
         return visits;
     }
@@ -33,92 +93,93 @@ public class Patient {
         this.visits = visits;
     }
 
-    // Getter and Setter methods for age
     public int getAge() {
+        loadData();
         return age;
     }
 
     public void setAge(int age) {
         this.age = age;
-    }
-    
-    public void setBirthday(String birthday) {
-    	this.birthday = birthday;
-    }
-    
-    public String getBirthday() {
-    	return birthday;
+        saveData();
     }
 
-    // Getter and Setter methods for medicalHistory
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+        saveData();
+    }
+
+    public String getBirthday() {
+        loadData();
+        return birthday;
+    }
+
     public String getMedicalHistory() {
+        loadData();
         return medicalHistory;
     }
 
     public void setMedicalHistory(String medicalHistory) {
         this.medicalHistory = medicalHistory;
+        saveData();
     }
 
-    // Getter and Setter methods for password
     public String getPassword() {
+        loadData();
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+        saveData();
     }
 
-    // Getter and Setter methods for email
     public String getEmail() {
+        loadData();
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+        saveData();
     }
 
-    // Getter and Setter methods for phoneNumber
     public String getPhoneNumber() {
+        loadData();
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+        saveData();
     }
 
-    // Getter and Setter methods for insurance
     public String getInsurance() {
+        loadData();
         return insurance;
     }
 
     public void setInsurance(String insurance) {
         this.insurance = insurance;
+        saveData();
     }
 
-    // Getter and Setter methods for patientId
-    public String getPatientId() {
-        return patientId;
-    }
-
-    public void setPatientId(String patientId) {
-        this.patientId = patientId;
-    }
-
-    // Getter and Setter methods for firstName
     public String getFirstName() {
+        loadData();
         return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+        saveData();
     }
 
-    // Getter and Setter methods for lastName
     public String getLastName() {
+        loadData();
         return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+        saveData();
     }
 }
