ccbf55de8fed89bc2be0ef649e2ff50414d06189
KaitMcCain <163193486+KaitMcCain@users.noreply.github.com>
2024-04-18 18:41:24 -0700

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,20 +1,92 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Patient {
-    private String patientId;
-    private String patientName;
+    
     private List<Visit> visits;
+    
+    //intake
+    private int age;
+    private String medicalHistory;
 
-    public Patient(String patientId, String patientName) {
-        this.patientId = patientId;
-        this.patientName = patientName;
-        this.visits = new ArrayList<>();
+    //from Sign Up page 
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String insurance;
+    private String patientId;
+    private String firstName;
+    private String lastName;
+    
+    public void addVisit(Visit visit) {
+    	visits.add(visit);
     }
 
-    // Getters and setters
+    // Getter and Setter methods for visits
+    public List<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(List<Visit> visits) {
+        this.visits = visits;
+    }
+
+    // Getter and Setter methods for age
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter and Setter methods for medicalHistory
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+
+    public void setMedicalHistory(String medicalHistory) {
+        this.medicalHistory = medicalHistory;
+    }
+
+    // Getter and Setter methods for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Getter and Setter methods for email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter and Setter methods for phoneNumber
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getter and Setter methods for insurance
+    public String getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(String insurance) {
+        this.insurance = insurance;
+    }
+
+    // Getter and Setter methods for patientId
     public String getPatientId() {
         return patientId;
     }
@@ -23,19 +95,21 @@ public class Patient {
         this.patientId = patientId;
     }
 
-    public String getPatientName() {
-        return patientName;
+    // Getter and Setter methods for firstName
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setPatientName(String patientName) {
-        this.patientName = patientName;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public List<Visit> getVisits() {
-        return visits;
+    // Getter and Setter methods for lastName
+    public String getLastName() {
+        return lastName;
     }
 
-    public void addVisit(Visit visit) {
-        visits.add(visit);
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
